9640304d936aa0106a7565d6158964b0ba57e22e
Nicholas A Christensen <nickachristensen@gmail.com>
2021-08-24 13:14:25 -0400

    SEARCH WORKS! WE ROCK!

--- index.js
+++ index.js
@@ -2,35 +2,58 @@
 
 const button = document.getElementById('submitButton')
 const list = document.getElementById('fetchedData')
-const form = document.getElementById('routine')
+
 const APIURL = "https://exercisedb.p.rapidapi.com/exercises"
 
-fetch(APIURL, {
-   "method": "GET",
-   "headers": {
-       "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-       "x-rapidapi-key": "9074bf701emsh2b8696dac91ac18p161ae0jsn7153acb84d2d"
-   }
-})
-.then(resp => resp.json())
-.then(array => {
-    array.forEach(obj => {
-        renderObj(obj)
-    })
-})
+// fetch(APIURL, {
+//    "method": "GET",
+//    "headers": {
+//        "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+//        "x-rapidapi-key": "9074bf701emsh2b8696dac91ac18p161ae0jsn7153acb84d2d"
+//    }
+// })
+// .then(resp => resp.json())
+// .then(arr => {
+//     arr.forEach(obj => {
+//         renderExercises(obj)
+// });
+// })
 
-function renderObj(obj) {
+// user searches api for exercises
+// only pull like matches
+
+
+
+function renderExercises(obj) {
     const newExercise = document.createElement('li')
-    newExercise.textContent = obj.target
-    newExercise.id = obj.target
+    newExercise.textContent = obj.name
     list.append(newExercise)
 
-    newExercise.addEventListener('click', event => {
-        const exerciseId = obj.id
-        fetch(`https://exercisedb.p.rapidapi.com/exercises/${exerciseId}`)
-        .then((resp) => resp.json())
-        .then(obj => {
-            featureExercise(obj)
-        })
-    })
 }
+const searchForm = document.getElementById('search-form')
+   
+    searchForm.addEventListener('submit', event => {
+        event.preventDefault()
+        const submitValue = event.target.search.value
+        console.log(submitValue)
+        
+        fetch(`https://exercisedb.p.rapidapi.com/exercises/target/${submitValue}`, {
+        "method": "GET",
+           "headers": {
+               "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+               "x-rapidapi-key": "9074bf701emsh2b8696dac91ac18p161ae0jsn7153acb84d2d"
+           }
+        })
+        .then(resp => resp.json())
+        .then(arr => {
+            arr.forEach(obj => {
+                if (submitValue !== obj.target.value) {
+                    renderExercises(obj);
+                }
+                else {
+                    alert('Please search again');
+                }
+                console.log(obj.length)
+     })
+    })
+})

1ca963092df445ada5b10b9f6d38cb57c6197ce2
+ <+>
2021-08-24 06:52:27 -0700

    added fetch API

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // JS GOES HERE //
 
 const button = document.getElementById('submitButton')
-
 const form = document.getElementById('routine')
+const APIURL = "https://exercisedb.p.rapidapi.com/exercises"
 
+fetch(APIURL, {
+   "method": "GET",
+   "headers": {
+       "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+       "x-rapidapi-key": "9074bf701emsh2b8696dac91ac18p161ae0jsn7153acb84d2d"
+   }
+})
+.then(resp => resp.json())
+.then(console.log)

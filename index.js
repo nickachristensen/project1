645ba8a6c20571fea80d239469eeab88bac783c8
Nicholas A Christensen <nickachristensen@gmail.com>
2021-08-26 12:16:27 -0400

    all exericse lis can now be clicked to populate featured

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// JS GOES HERE //
-
-const button = document.getElementById('submitButton')
-const list = document.getElementById('fetchedData')
 const APIURL = "https://exercisedb.p.rapidapi.com/exercises"
+const button = document.getElementById('submitButton')
+const fetchedExerciseList = document.getElementById('fetchedData')
+const routine = document.getElementById('routine')
+const searchForm = document.getElementById('search-form')
 
 fetch(APIURL, {
    "method": "GET",
@@ -16,81 +16,289 @@ fetch(APIURL, {
     console.log(arr)
     let uniqueTargets = [...new Set(arr.map(exercise => exercise.target))]
     uniqueTargets.forEach(targetString => {
-
         const exerciseSearches = document.createElement('option')
         exerciseSearches.textContent = targetString
         const searchList = document.querySelector("#exercises")
-        searchList.append(exerciseSearches)
-        
+        searchList.append(exerciseSearches)       
     })
 })
-
-
 
 function renderExercises(obj) {
     const newExercise = document.createElement('li')
     newExercise.textContent = obj.name
-    list.append(newExercise)
+    fetchedExerciseList.append(newExercise)
 
 }
-const searchForm = document.getElementById('search-form')
 
-    searchForm.addEventListener('submit', event => {
-        event.preventDefault()
-        
-        while (list.firstChild) {
-            list.removeChild(list.firstChild);
-        }
+searchForm.addEventListener('submit', event => {
+    event.preventDefault()
+    while (fetchedExerciseList.firstChild) {
+        fetchedExerciseList.removeChild(fetchedExerciseList.firstChild);
+    }
 
-        const submitValue = event.target[0].value
-        console.log(submitValue)
-        
+const submitValue = event.target[0].value
+console.log(submitValue)
+
 fetch(`https://exercisedb.p.rapidapi.com/exercises/target/${submitValue}`, {
-        "method": "GET",
-           "headers": {
-               "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-               "x-rapidapi-key": "9074bf701emsh2b8696dac91ac18p161ae0jsn7153acb84d2d"
-           }
-        })   
-        .then(resp => resp.json())
-        .then(arr => {
-            arr.forEach(obj => {
-                 renderExercises(obj);
-                 console.log(obj.length)
-            })
-         searchForm.reset()             
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+         "x-rapidapi-key": "9074bf701emsh2b8696dac91ac18p161ae0jsn7153acb84d2d"
+        }
+    })   
+    .then(resp => resp.json())
+    .then(arr => {
+        arr.forEach(obj => {
+            renderExercises(obj);
+            console.log(obj.length)
+                 
+        })     
+        searchForm.reset()             
      })
 })
 
 
+const li = document.querySelector('ul#fetchedData')
+li.addEventListener('click', e => {
+    const featureExercise = e.target.textContent
+    renderFeaturedExercise()
+    console.log(featureExercise)
 
+function renderFeaturedExercise() {
+    fetch(`https://exercisedb.p.rapidapi.com/exercises/name/${featureExercise}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+            "x-rapidapi-key": "9074bf701emsh2b8696dac91ac18p161ae0jsn7153acb84d2d"
+        }
+    })
+    .then(response => response.json())
+    .then(arr => {
+        arr.forEach(obj => {
+            const featureBox = document.getElementById('feature')
+            featureBox.innerHTML = ""
+            const featureImg = document.createElement('img')
+            featureImg.src = obj.gifUrl
+            featureBox.append(featureImg)
+
+            const featureTarget = document.createElement('h3')
+            featureTarget.textContent = "Target muscle group: " + obj.target
+            featureBox.append(featureTarget)
+
+            const featureName = document.createElement('h3')
+            featureName.textContent = "Exercise: " + obj.name
+            featureBox.append(featureName)
+
+            const featureEquip = document.createElement('h3')
+            featureEquip.textContent = "Equipment: " + obj.equipment
+            featureBox.append(featureEquip)
+
+            let btn = document.createElement('button')
+            btn.innerHTML = "Add to Routine"
+            featureBox.appendChild(btn)
+
+            btn.addEventListener('click', event => {
+                
+                while (featureBox.firstChild) {
+                    featureBox.removeChild(featureBox.firstChild);
+                }
+
+                const routine = document.getElementById('myRoutine')
+                const newEx = document.createElement('li')
+                newEx.textContent = obj.name
+                newEx.id = obj.id
+                routine.append(newEx)
+
+                const xBtn = document.createElement('button')
+                xBtn.innerHTML = "Remove"
+                newEx.append(xBtn)
+
+                xBtn.addEventListener('click', e => {
+                    newEx.remove(e.target.parentElement)
+                })
+            })
+    })
+})
+}
+})
+
+const routineUl = document.getElementById('myRoutine')
+routineUl.addEventListener('click', e => {
+    const featureExercise = e.target.childNodes[0].textContent
+    renderFeaturedExercise()
+    console.log(featureExercise)
+
+function renderFeaturedExercise() {
+    fetch(`https://exercisedb.p.rapidapi.com/exercises/name/${featureExercise}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+            "x-rapidapi-key": "9074bf701emsh2b8696dac91ac18p161ae0jsn7153acb84d2d"
+        }
+    })
+    .then(response => response.json())
+    .then(arr => {
+        arr.forEach(obj => {
+            const featureBox = document.getElementById('feature')
+            featureBox.innerHTML = ""
+            const featureImg = document.createElement('img')
+            featureImg.src = obj.gifUrl
+            featureBox.append(featureImg)
+
+            const featureTarget = document.createElement('h3')
+            featureTarget.textContent = "Target muscle group: " + obj.target
+            featureBox.append(featureTarget)
+
+            const featureName = document.createElement('h3')
+            featureName.textContent = "Exercise: " + obj.name
+            featureBox.append(featureName)
+
+            const featureEquip = document.createElement('h3')
+            featureEquip.textContent = "Equipment: " + obj.equipment
+            featureBox.append(featureEquip)
+
+            let btn = document.createElement('button')
+            btn.textContent = "Add to Routine"
+            featureBox.appendChild(btn)
+
+            btn.addEventListener('click', event => {
+                
+                while (featureBox.firstChild) {
+                    featureBox.removeChild(featureBox.firstChild);
+                }
+
+                const routine = document.getElementById('myRoutine')
+                const newEx = document.createElement('li')
+                newEx.textContent = obj.name
+                newEx.id = obj.id
+                routine.append(newEx)
+
+                const xBtn = document.createElement('button')
+                xBtn.textContent = "Remove"
+                newEx.append(xBtn)
+
+                xBtn.addEventListener('click', e => {
+                    newEx.remove(e.target.parentElement)
+                })
+            })
+    })
+})
+}
+})
+
+
+
+
+// const routineBox = document.getElementById('myRoutine')
+
+// const favourite = document.createElement('button')
+// favourite.id = "favouriteBtn"
+// favourite.innerHTML = "SAVE"
+// routineBox.append(favourite)
+
+// favourite.addEventListener('click', e=> {
+//     const favouriteBox = document.querySelector('div.favourites')
+//     const favourited = document.createElement('li')
+//     favouriteBox.append(favourited)
+//     favourited.textContent = document.getElementsByName('myRoutine')
+//     console.log(favouriteBox)
+// })
  
-        
+const routineBox = document.getElementById('myRoutine')
+const favourite = document.createElement('button')
+favourite.id = "favouriteBtn"
+favourite.innerHTML = "SAVE"
+routineBox.append(favourite)
+favourite.addEventListener('click', e=> {
+    const favouriteBox = document.querySelector('div.favourites')
+    const favourited = document.createElement('li')
+    const listedItems = document.querySelectorAll('#myRoutine li')
+    // let listArray = []
+    for (let i=0; i<listedItems.length; i++){
+        favouriteBox.appendChild(listedItems[i])
+    }
+})
 
-//  const promise1 = fetch(`https://exercisedb.p.rapidapi.com/exercises/target/${submitValue}`, {
-// 	    "method": "GET",
-// 	     "headers": {
-// 		    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-// 		    "x-rapidapi-key": "9f7133ba3dmsh3c132d75638fd83p14a77bjsna10aef0a38ad"
-// 	}
-// })
+const favouriteBox = document.querySelector('div.favourites')
+favouriteBox.addEventListener('click', e => {
+    const featureExercise = e.target.childNodes[0].textContent
+    renderFeaturedExercise()
+    console.log(featureExercise)
 
-//     const promise2 = fetch(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${submitValue}`, {
-// 	    "method": "GET",
-// 	     "headers": {
-// 		    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-// 		    "x-rapidapi-key": "9f7133ba3dmsh3c132d75638fd83p14a77bjsna10aef0a38ad"
-// 	}
-// })
-    
-//     const promise3 = fetch(`https://exercisedb.p.rapidapi.com/exercises/equipment/${submitValue}`, {
-// 	    "method": "GET",
-// 	     "headers": {
-// 		    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-// 		    "x-rapidapi-key": "9f7133ba3dmsh3c132d75638fd83p14a77bjsna10aef0a38ad"
-// 	}
-// })
+function renderFeaturedExercise() {
+    fetch(`https://exercisedb.p.rapidapi.com/exercises/name/${featureExercise}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+            "x-rapidapi-key": "9074bf701emsh2b8696dac91ac18p161ae0jsn7153acb84d2d"
+        }
+    })
+    .then(response => response.json())
+    .then(arr => {
+        arr.forEach(obj => {
+            const featureBox = document.getElementById('feature')
+            featureBox.innerHTML = ""
+            const featureImg = document.createElement('img')
+            featureImg.src = obj.gifUrl
+            featureBox.append(featureImg)
 
-//     const promises = [promise1, promise2, promise3];
-   
-//    Promise.race(promises)
+            const featureTarget = document.createElement('h3')
+            featureTarget.textContent = "Target muscle group: " + obj.target
+            featureBox.append(featureTarget)
+
+            const featureName = document.createElement('h3')
+            featureName.textContent = "Exercise: " + obj.name
+            featureBox.append(featureName)
+
+            const featureEquip = document.createElement('h3')
+            featureEquip.textContent = "Equipment: " + obj.equipment
+            featureBox.append(featureEquip)
+
+            let btn = document.createElement('button')
+            btn.textContent = "Add to Routine"
+            featureBox.appendChild(btn)
+
+            btn.addEventListener('click', event => {
+                
+                while (featureBox.firstChild) {
+                    featureBox.removeChild(featureBox.firstChild);
+                }
+
+                const routine = document.getElementById('myRoutine')
+                const newEx = document.createElement('li')
+                newEx.textContent = obj.name
+                newEx.id = obj.id
+                routine.append(newEx)
+
+                const xBtn = document.createElement('button')
+                xBtn.textContent = "Remove"
+                newEx.append(xBtn)
+
+                xBtn.addEventListener('click', e => {
+                    newEx.remove(e.target.parentElement)
+                })
+            })
+    })
+})
+}
+})
+
+// const routineBox = document.getElementById('myRoutine')
+// const favourite = document.createElement('button')
+// favourite.id = "favouriteBtn"
+// favourite.innerHTML = "SAVE"
+// routineBox.append(favourite)
+// favourite.addEventListener('click', e=> {
+//     const favouriteBox = document.querySelector('div.favourites')
+//     const favourited = document.createElement('li')
+//     const listedItems = document.querySelectorAll('#myRoutine li')
+//     for (let i=0; i<listedItems.length; i++){
+//         let listArray = []
+//         listArray.push(listedItems[i])
+//         listArray.id = [i]
+//         for(obj of listArray){
+//             console.log(obj.textContent)
+//         }
+//         favouriteBox.append()
+//     }
+// })

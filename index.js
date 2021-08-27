925506297ac73cc1ce5af5bfd987989c13c909ae
Nicholas A Christensen <nickachristensen@gmail.com>
2021-08-27 10:41:12 -0400

    Cleaned up code

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const APIURL = "https://exercisedb.p.rapidapi.com/exercises"
-const button = document.getElementById('submitButton')
 const fetchedExerciseList = document.getElementById('fetchedData')
-const routine = document.getElementById('routine')
 const searchForm = document.getElementById('search-form')
+const routineUl = document.getElementById('myRoutine')
 
 fetch(APIURL, {
    "method": "GET",
@@ -13,7 +12,6 @@ fetch(APIURL, {
 })
 .then(resp => resp.json())
 .then(arr => {
-    console.log(arr)
     let uniqueTargets = [...new Set(arr.map(exercise => exercise.target))]
     uniqueTargets.forEach(targetString => {
         const exerciseSearches = document.createElement('option')
@@ -23,22 +21,18 @@ fetch(APIURL, {
     })
 })
 
-function renderExercises(obj) {
+const renderExercises = obj => {
     const newExercise = document.createElement('li')
     newExercise.textContent = obj.name
     fetchedExerciseList.append(newExercise)
-
-}
+    }
 
 searchForm.addEventListener('submit', event => {
     event.preventDefault()
     while (fetchedExerciseList.firstChild) {
         fetchedExerciseList.removeChild(fetchedExerciseList.firstChild);
     }
-
 const submitValue = event.target[0].value
-console.log(submitValue)
-
 fetch(`https://exercisedb.p.rapidapi.com/exercises/target/${submitValue}`, {
     "method": "GET",
     "headers": {
@@ -49,20 +43,17 @@ fetch(`https://exercisedb.p.rapidapi.com/exercises/target/${submitValue}`, {
     .then(resp => resp.json())
     .then(arr => {
         arr.forEach(obj => {
-            renderExercises(obj);
-            console.log(obj.length)
-                 
+            renderExercises(obj);        
         })     
         searchForm.reset()             
-     })
+    })
 })
 
 
 const li = document.querySelector('ul#fetchedData')
 li.addEventListener('click', e => {
-    const featureExercise = e.target.textContent
+const featureExercise = e.target.textContent
     renderFeaturedExercise()
-    console.log(featureExercise)
 
 function renderFeaturedExercise() {
     fetch(`https://exercisedb.p.rapidapi.com/exercises/name/${featureExercise}`, {
@@ -98,16 +89,13 @@ function renderFeaturedExercise() {
             featureBox.appendChild(btn)
 
             btn.addEventListener('click', event => {
-                
                 while (featureBox.firstChild) {
                     featureBox.removeChild(featureBox.firstChild);
                 }
-
-                const routine = document.getElementById('myRoutine')
                 const newEx = document.createElement('li')
                 newEx.textContent = obj.name
                 newEx.id = obj.id
-                routine.append(newEx)
+                routineUl.append(newEx)
 
                 const xBtn = document.createElement('button')
                 xBtn.innerHTML = "Remove"
@@ -117,16 +105,14 @@ function renderFeaturedExercise() {
                     newEx.remove(e.target.parentElement)
                 })
             })
+        })
     })
-})
-}
-})
+}})
 
-const routineUl = document.getElementById('myRoutine')
+
 routineUl.addEventListener('click', e => {
-    const featureExercise = e.target.childNodes[0].textContent
+const featureExercise = e.target.childNodes[0].textContent
     renderFeaturedExercise()
-    console.log(featureExercise)
 
 function renderFeaturedExercise() {
     fetch(`https://exercisedb.p.rapidapi.com/exercises/name/${featureExercise}`, {
@@ -162,16 +148,14 @@ function renderFeaturedExercise() {
             featureBox.appendChild(btn)
 
             btn.addEventListener('click', event => {
-                
                 while (featureBox.firstChild) {
                     featureBox.removeChild(featureBox.firstChild);
                 }
 
-                const routine = document.getElementById('myRoutine')
                 const newEx = document.createElement('li')
                 newEx.textContent = obj.name
                 newEx.id = obj.id
-                routine.append(newEx)
+                routineUl.append(newEx)
 
                 const xBtn = document.createElement('button')
                 xBtn.textContent = "Remove"
@@ -181,39 +165,17 @@ function renderFeaturedExercise() {
                     newEx.remove(e.target.parentElement)
                 })
             })
+        })
     })
-})
-}
-})
+}})
 
-
-
-
-// const routineBox = document.getElementById('myRoutine')
-
-// const favourite = document.createElement('button')
-// favourite.id = "favouriteBtn"
-// favourite.innerHTML = "SAVE"
-// routineBox.append(favourite)
-
-// favourite.addEventListener('click', e=> {
-//     const favouriteBox = document.querySelector('div.favourites')
-//     const favourited = document.createElement('li')
-//     favouriteBox.append(favourited)
-//     favourited.textContent = document.getElementsByName('myRoutine')
-//     console.log(favouriteBox)
-// })
- 
-const routineBox = document.getElementById('myRoutine')
 const favourite = document.createElement('button')
 favourite.id = "favouriteBtn"
 favourite.innerHTML = "SAVE"
-routineBox.append(favourite)
+routineUl.append(favourite)
 favourite.addEventListener('click', e=> {
     const favouriteBox = document.querySelector('div.favourites')
-    const favourited = document.createElement('li')
     const listedItems = document.querySelectorAll('#myRoutine li')
-    // let listArray = []
     for (let i=0; i<listedItems.length; i++){
         favouriteBox.appendChild(listedItems[i])
     }
@@ -259,16 +221,14 @@ function renderFeaturedExercise() {
             featureBox.appendChild(btn)
 
             btn.addEventListener('click', event => {
-                
                 while (featureBox.firstChild) {
                     featureBox.removeChild(featureBox.firstChild);
                 }
 
-                const routine = document.getElementById('myRoutine')
                 const newEx = document.createElement('li')
                 newEx.textContent = obj.name
                 newEx.id = obj.id
-                routine.append(newEx)
+                routineUl.append(newEx)
 
                 const xBtn = document.createElement('button')
                 xBtn.textContent = "Remove"
@@ -278,27 +238,6 @@ function renderFeaturedExercise() {
                     newEx.remove(e.target.parentElement)
                 })
             })
+        })
     })
-})
-}
-})
-
-// const routineBox = document.getElementById('myRoutine')
-// const favourite = document.createElement('button')
-// favourite.id = "favouriteBtn"
-// favourite.innerHTML = "SAVE"
-// routineBox.append(favourite)
-// favourite.addEventListener('click', e=> {
-//     const favouriteBox = document.querySelector('div.favourites')
-//     const favourited = document.createElement('li')
-//     const listedItems = document.querySelectorAll('#myRoutine li')
-//     for (let i=0; i<listedItems.length; i++){
-//         let listArray = []
-//         listArray.push(listedItems[i])
-//         listArray.id = [i]
-//         for(obj of listArray){
-//             console.log(obj.textContent)
-//         }
-//         favouriteBox.append()
-//     }
-// })
+}})
